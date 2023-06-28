c5c9d2e730a9af9e03e8823e3abff01c53c1d89a
Yicheng Zou <59714064+YichengZou626@users.noreply.github.com>
2023-06-28 11:55:57 -0400

    Add files via upload

---
+++ TractSeg/tractseg/experiments/custom/my_custom_experiment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from tractseg.experiments.tract_seg import Config as TractSegConfig
+#change tract_seg to endings_seg or peak_reg
+
+class Config(TractSegConfig):
+    EXP_NAME = os.path.basename(__file__).split(".")[0]
+
+    DATASET_FOLDER = "/nas/longleaf/home/zyc626/TractSeg/tractseg/preproc"      # name of folder that contains all the preprocessed subjects (each subject has its own folder with the name of the subjectID)
+    FEATURES_FILENAME = "mrtrix_peaks"  # filename of nifti file (*.nii.gz) without file ending; mrtrix CSD peaks; shape: [x,y,z,9]; one file for each subject
+    LABELS_FILENAME = "bundle_masks"
---
+++ TractSeg/tractseg/experiments/peak_reg.py
@@ -0,0 +1,21 @@
+
+from tractseg.experiments.base import Config as BaseConfig
+
+
+class Config(BaseConfig):
+
+    EXPERIMENT_TYPE = "peak_regression"
+    CLASSES = "All_Part1" #20
+    BATCH_SIZE = 44
+    LOSS_WEIGHT = 10
+    LOSS_WEIGHT_LEN = 400 # nr of epochs 400
+    LABELS_TYPE = "float"
+    TRAINING_SLICE_DIRECTION = "y"
+    GET_PROBS = True
+    INFO_2 = "using AngleLengthLoss, PeakLengthDice"
+    UPSAMPLE_TYPE = "nearest"
+    LOSS_FUNCTION = "angle_length_loss"
+    FEATURES_FILENAME = "12g90g270g_CSD_BX"
+    NUM_EPOCHS = 250  #250
+    METRIC_TYPES = ["loss", "f1_macro", "angle_err"]
+    THRESHOLD = 0.01
